14f38dbfd0c53f238c5d733e2f3244ccf09c1d5f
Evan Woods <22337004+efwoods@users.noreply.github.com>
2025-08-04 20:31:20 -0400

    using static route rather than dynamic ngrok url for cloud rather than local deployment

--- src/context/NgrokAPIContext.jsx
+++ src/context/NgrokAPIContext.jsx
@@ -8,49 +8,53 @@ export const NgrokUrlProvider = ({ children }) => {
   const [ngrokWsUrl, setNgrokWsUrl] = useState(null);
   const [ngrokHttpsUrl, setNgrokHttpsUrl] = useState(null);
 
-  useEffect(() => {
-    const fetchNgrokUrl = async () => {
-      if (
-        !import.meta.env.VITE_GITHUB_FETCH_URL_ROOT ||
-        !import.meta.env.VITE_GITHUB_GIST_ID ||
-        !import.meta.env.VITE_GITHUB_GIST_FILENAME
-      ) {
-        console.error('Missing VITE environment variables');
-        return;
-      }
+  // Using a known subdomain with proxy
+  setNgrokWsUrl('wss://api.neuralnexus.site');
+  setNgrokHttpsUrl('https://api.neuralnexus.site');
 
-      try {
-        const fetch_url =
-          import.meta.env.VITE_GITHUB_FETCH_URL_ROOT +
-          import.meta.env.VITE_GITHUB_GIST_ID +
-          import.meta.env.VITE_GITHUB_GIST_FILENAME +
-          '?t=' +
-          Date.now();
+  // useEffect(() => {
+  //   const fetchNgrokUrl = async () => {
+  //     if (
+  //       !import.meta.env.VITE_GITHUB_FETCH_URL_ROOT ||
+  //       !import.meta.env.VITE_GITHUB_GIST_ID ||
+  //       !import.meta.env.VITE_GITHUB_GIST_FILENAME
+  //     ) {
+  //       console.error('Missing VITE environment variables');
+  //       return;
+  //     }
 
-        console.log('fetch_url:', fetch_url);
+  //     try {
+  //       const fetch_url =
+  //         import.meta.env.VITE_GITHUB_FETCH_URL_ROOT +
+  //         import.meta.env.VITE_GITHUB_GIST_ID +
+  //         import.meta.env.VITE_GITHUB_GIST_FILENAME +
+  //         '?t=' +
+  //         Date.now();
 
-        console.log('Calling NGROK FETCH URL FROM NGROKAPICONTEXT');
-        const response = await fetch(fetch_url, { cache: 'no-store' });
-        const data = await response.text();
-        console.log('data:', data);
-        console.log('response:', response);
-        const cleanHttpUrl = data.trim().replace(/^wss:\/\//, 'https://');
-        console.log('ngrok_url:', data);
-        console.log('cleanHttpUrl:', cleanHttpUrl);
-        if (!cleanHttpUrl.startsWith('https')) {
-          console.error('Invalid URL fetched:', cleanHttpUrl);
-          return;
-        }
-        setNgrokWsUrl(data);
-        setNgrokHttpsUrl(cleanHttpUrl);
-        setNgrokUrls(cleanHttpUrl, data);
-      } catch (error) {
-        console.error('Error fetching ngrok URL:', error);
-      }
-    };
+  //       console.log('fetch_url:', fetch_url);
 
-    fetchNgrokUrl();
-  }, []);
+  //       console.log('Calling NGROK FETCH URL FROM NGROKAPICONTEXT');
+  //       const response = await fetch(fetch_url, { cache: 'no-store' });
+  //       const data = await response.text();
+  //       console.log('data:', data);
+  //       console.log('response:', response);
+  //       const cleanHttpUrl = data.trim().replace(/^wss:\/\//, 'https://');
+  //       console.log('ngrok_url:', data);
+  //       console.log('cleanHttpUrl:', cleanHttpUrl);
+  //       if (!cleanHttpUrl.startsWith('https')) {
+  //         console.error('Invalid URL fetched:', cleanHttpUrl);
+  //         return;
+  //       }
+  //       setNgrokWsUrl(data);
+  //       setNgrokHttpsUrl(cleanHttpUrl);
+  //       setNgrokUrls(cleanHttpUrl, data);
+  //     } catch (error) {
+  //       console.error('Error fetching ngrok URL:', error);
+  //     }
+  //   };
+
+  //   fetchNgrokUrl();
+  // }, []);
 
   return (
     <APIContext.Provider value={{ ngrokHttpsUrl, ngrokWsUrl }}>
