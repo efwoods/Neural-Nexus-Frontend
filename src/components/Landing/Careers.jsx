a1b9aadd8429cdf6fe14e358ac55a07cbb68702f
Evan Woods <22337004+efwoods@users.noreply.github.com>
2025-10-30 21:15:19 -0400

    created a waitlist

--- src/components/Landing/Careers.jsx
+++ src/components/Landing/Careers.jsx
@@ -8,16 +8,19 @@ export default function Careers() {
       className="py-16 bg-gradient-to-b bg-purple-900 to-purple-400 text-white"
     >
       <div className="container mx-auto">
-        <h2 className="text-3xl font-bold text-center mb-8">Join Our Team</h2>
+        <h2 className="text-3xl font-bold text-center mb-8">
+          Join Our Waitlist
+        </h2>
         <p className="text-lg text-center">
-          We’re hiring passionate individuals to join our mission-driven team.
-          Apply now to shape the future of AI.
+          We’re seeking passionate early-adopters to pilot our cutting-edge
+          software. Send an email now to join the waitlist and shape the future
+          of AI!
         </p>
         <a
-          href="mailto:hiring@neuralnexus.site"
+          href="mailto:waitlist@neuralnexus.site"
           className="block text-center text-white hover:underline hover:text-purple-200 transition"
         >
-          hiring@neuralnexus.site
+          waitlist@neuralnexus.site
         </a>
       </div>
     </section>
